080643d9f09850357489a5599203becb7f0c25d9
Jordan <jmowlai@gmail.com>
2022-11-03 19:48:35 +1100

    playRound function complete

--- code.js
+++ code.js
@@ -6,4 +6,43 @@ const moves = [
 
 getComputerChoice = () => moves[Math.floor(Math.random() * moves.length)]
 
-console.log(getComputerChoice())
+const playerSelection = "rock"
+const computerSelection = getComputerChoice()
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toUpperCase() == "ROCK") {
+        if (computerSelection == "Rock") {
+            return "Tie, you both chose Rock"
+        }
+        else if (computerSelection == "Paper"){
+            return "You Lose! Paper beats Rock"
+        }
+        else {
+            return "You Win! Rock beats scissors"
+        }
+    }
+    if (playerSelection.toUpperCase() == "PAPER") {
+        if (computerSelection == "Rock") {
+            return "You Win!, Paper beats Rock"
+        }
+        else if (computerSelection == "Paper"){
+            return "Tie, you both chose Paper"
+        }
+        else {
+            return "You Lose! Scissors beats Paper"
+        }
+    }
+    if (playerSelection.toUpperCase() == "SCISSORS") {
+        if (computerSelection == "Rock") {
+            return "You Lose!, Rock beats Scissors"
+        }
+        else if (computerSelection == "Paper"){
+            return "You Win, Scissors beats Paper"
+        }
+        else {
+            return "Tie, you both chose Scissors"
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection))
