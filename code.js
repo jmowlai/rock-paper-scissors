407c44390118d9277b69f175cf2b48366ddeb918
Jordan <jmowlai@gmail.com>
2022-11-03 20:33:47 +1100

    Finish Project: Rock Paper Scissors

--- code.js
+++ code.js
@@ -1,14 +1,3 @@
-const moves = [
-    'Rock',
-    'Paper',
-    'Scissors'
-]
-
-getComputerChoice = () => moves[Math.floor(Math.random() * moves.length)]
-
-const playerSelection = "rock"
-const computerSelection = getComputerChoice()
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toUpperCase() == "ROCK") {
         if (computerSelection == "Rock") {
@@ -45,4 +34,41 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-console.log(playRound(playerSelection, computerSelection))
+const moves = [
+    'Rock',
+    'Paper',
+    'Scissors'
+]
+
+getComputerChoice = () => moves[Math.floor(Math.random() * moves.length)]
+
+function game() {
+    let player = 0;
+    let computer = 0;
+    let result;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("What is your move?");
+        const computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection)
+        console.log(result)
+        result = result.toUpperCase()
+        if (result.includes("WIN") == true) {
+            ++player;
+        }
+        else if (result.includes("LOSE") == true) {
+            ++computer;
+        }
+    }
+    if (player == computer) {
+        console.log("Tie! Player: " + player + " Computer: " + computer);
+    }
+    else if (player > computer){
+        console.log("Win! Player: " + player + " Computer: " + computer);
+
+    }
+    else {
+        console.log("Lose! Player: " + player + " Computer: " + computer);
+    }
+}
+
+game()
